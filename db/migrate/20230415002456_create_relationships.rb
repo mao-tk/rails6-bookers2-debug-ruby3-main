24fa22fd9204f958c0e9126013c83f6efff7297c
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-15 04:18:23 +0000

    [Add]フォロー・フォロワー機能

--- db/migrate/20230415002456_create_relationships.rb
+++ db/migrate/20230415002456_create_relationships.rb
@@ -2,8 +2,10 @@ class CreateRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :relationships do |t|
 
-      t.integer :user_id
-      t.integer :book_id
+      # フォローするユーザーid
+      t.integer :follower_id
+      # フォローされるユーザーid
+      t.integer :followed_id
       t.timestamps
     end
   end
