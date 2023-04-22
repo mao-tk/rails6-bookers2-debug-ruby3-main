c6d0549585816a6cd886d60427fc5032b5491a6c
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-22 06:33:20 +0000

    [Add]DM機能

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  
+
+  resources :chats, only: [:show, :create]
+
   get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
