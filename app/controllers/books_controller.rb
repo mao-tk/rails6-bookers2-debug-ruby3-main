b6469df1e9f43ad58a56fec6487d231710cb5506
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-22 07:04:39 +0000

    [Add]閲覧数表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,15 +3,19 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
+    end
     @book_new = Book.new
     @book_comment = BookComment.new
+
   end
 
   def index
+    @book = Book.new
     to = Time.current.at_end_of_day
     from = (to - 6.day).at_beginning_of_day
     @books = Book.all.sort_by {|x| x.favorites.where(created_at: from...to).size}.reverse
-    @book = Book.new
   end
 
   def create
