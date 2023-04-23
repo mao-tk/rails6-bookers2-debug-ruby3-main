adf36b86eb5922b281cf2b1a850f45ca9bb78c46
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-23 01:52:08 +0000

    [finish]応用課題7ｃ

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,16 +1,54 @@
 class GroupsController < ApplicationController
+  before_action :is_matching_owner, only: [:edit, :update]
+
   def new
     @group = Group.new
   end
-  
+
   def create
-    @group = Group.new
-    
+    @group = Group.new(group_params)
+    @group.owner_id = current_user.id
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def index
+    @book = Book.new
+    @groups = Group.all
   end
 
   def show
+    @book = Book.new
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :introduction, :image)
+  end
+
+  def is_matching_owner
+    group = Group.find(params[:id])
+    user = group.owner_id
+    unless user == current_user.id
+      redirect_to groups_path
+    end
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -5,8 +5,13 @@ class Group < ApplicationRecord
   has_one_attached :image
 
   validates :name, presence: true
+  validates :introduction, presence: true
 
-  def get_image
-    (image.attached?) ? image : 'no_image.jpg'
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/noimage-group.png')
+      image.attach(io: File.open(file_path), filename: 'no-image.png', content_type: 'image/png')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
   end
 end
