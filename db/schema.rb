c943928bc2397ff6c939df93593ed94d45f070b1
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-05-01 09:38:00 +0000

    [Add]タグ検索ボタン・タグ投稿フォーム・一覧・詳細ページへのタグの表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_26_120252) do
+ActiveRecord::Schema.define(version: 2023_05_01_092555) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2023_04_26_120252) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "star"
+    t.string "tag"
   end
 
   create_table "chats", force: :cascade do |t|
